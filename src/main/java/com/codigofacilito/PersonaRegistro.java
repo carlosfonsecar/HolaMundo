77feba68b66ccc87aeae7a4ac826c7b58c4fe799
Carlos Fonseca <cfonseca@transer.com.co>
2025-03-13 15:57:52 -0500

    Se quito la linea de codigo inservible

--- src/main/java/com/codigofacilito/PersonaRegistro.java
+++ src/main/java/com/codigofacilito/PersonaRegistro.java
@@ -3,8 +3,6 @@ package com.codigofacilito;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
-        //Se comento la liena que sobra CF
-        //System.out.println("esta instruccion es inservible");
         return "%s %s".formatted(nombre,apellidos);
     }
 }
