22750e44f6c32993fe7e416f19629f7136649f6d
Carlos Fonseca <cfonseca@transer.com.co>
2025-03-13 15:23:01 -0500

    Se agrego una linea de codigo inservible

--- src/main/java/com/codigofacilito/PersonaRegistro.java
+++ src/main/java/com/codigofacilito/PersonaRegistro.java
@@ -3,6 +3,7 @@ package com.codigofacilito;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
+        System.out.println("esta instruccion es inservible");
         return "%s %s".formatted(nombre,apellidos);
     }
 }
